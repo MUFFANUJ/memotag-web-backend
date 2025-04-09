4f5eac2793a0735c696940afb2ae8b754abb361e
ANUJ KUMAR SINGH <151390316+MUFFANUJ@users.noreply.github.com>
2025-04-09 10:32:58 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const transporter = require('./nodemailer-config');
+require('dotenv').config();
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors()); 
+
+app.post('/send-note', async (req, res) => {
+  const { to, subject, content } = req.body;
+
+  if (!to || !subject || !content) {
+    return res.status(400).json({ message: 'to, subject, and content are required' });
+  }
+
+  try {
+    await transporter.sendMail({
+      from: `"CONTACT FORM QUERY " <${process.env.EMAIL_USER}>`,
+      to: to,
+      subject: subject,
+      text: content,
+    });
+
+    res.status(200).json({ message: 'Email Sent Successfully!' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Failed to send Email' });
+  }
+});
+
+app.get("/hello",(req,res)=>{
+  res.send({message:"Its Working buddy!"})
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on http://localhost:${process.env.PORT}`);
+});
